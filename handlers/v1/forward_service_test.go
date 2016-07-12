40f70feb4a39bb0b2fac925dd382b9533b5cd36a
Marc Campbell <marc.e.campbell@gmail.com>
2016-07-11 18:15:07 -0700

    Forward services support namespaces with more than one element

---
+++ handlers/v1/forward_service_test.go
@@ -0,0 +1,46 @@
+package v1
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStripLeadingSlashIfPresent(t *testing.T) {
+	before := "/test"
+	after := stripLeadingSlashIfPresent(before)
+	assert.Equal(t, "test", after)
+
+	before = "test"
+	after = stripLeadingSlashIfPresent(before)
+	assert.Equal(t, "test", after)
+}
+
+func TestIsPathPrefix(t *testing.T) {
+	servicePath := "service"
+	requestPath := "/service/something"
+	ok := isPathPrefix(servicePath, requestPath)
+	assert.True(t, ok)
+
+	servicePath = "/service"
+	requestPath = "service/something"
+	ok = isPathPrefix(servicePath, requestPath)
+	assert.True(t, ok)
+
+	servicePath = "/service"
+	requestPath = "svc/something"
+	ok = isPathPrefix(servicePath, requestPath)
+	assert.False(t, ok)
+}
+
+func TestCreateForwardPath(t *testing.T) {
+	servicePath := "service"
+	requestPath := "/service/something"
+	forwardPath := createForwardPath(servicePath, requestPath)
+	assert.Equal(t, "/something", forwardPath)
+
+	servicePath = "service/test/something"
+	requestPath = "/service/test/something/one/two"
+	forwardPath = createForwardPath(servicePath, requestPath)
+	assert.Equal(t, "/one/two", forwardPath)
+}
