3ca2c701b820273bd0ee9b9856a64e04729ee8d0
Ethan Mosbaugh <ethan@replicated.com>
2023-06-06 09:30:18 -0700

    chore: upgrade to go 1.20 (#59)

--- app.go
+++ app.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"math/rand"
-	"runtime"
-	"time"
-
 	"github.com/premkit/premkit/commands"
 )
 
-func init() {
-	rand.Seed(time.Now().UTC().UnixNano())
-}
-
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
 	commands.Execute()
 }
